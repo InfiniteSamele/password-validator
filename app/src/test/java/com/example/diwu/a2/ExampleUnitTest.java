85536365ba094aa14a026fcdf0592597d2ab1d50
InfiniteSamele <32623904+InfiniteSamele@users.noreply.github.com>
2018-02-10 09:06:04 -0400

    Update ExampleUnitTest.java

--- app/src/test/java/com/example/diwu/a2/ExampleUnitTest.java
+++ app/src/test/java/com/example/diwu/a2/ExampleUnitTest.java
@@ -4,11 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+
 public class ExampleUnitTest {
     @Test
     public void check0() throws Exception {
@@ -20,14 +16,14 @@ public class ExampleUnitTest {
     @Test
     public void check1() {
         Validator check = new Validator();
-        assertEquals(1, check.validate("password"));//String = "password" (8 characters)
-        assertEquals(1, check.validate("qwertyu"));//String have 7 characters
+        assertEquals(1, check.validate("password"));
+        assertEquals(1, check.validate("qwertyu"));
     }
 
     @Test
     public void check2() {
         Validator check = new Validator();
-        assertEquals(2, check.validate("Password"));//String = "password" upcase
-        assertEquals(2, check.validate("qwertyui"));// 8 characters
+        assertEquals(2, check.validate("Password"));
+        assertEquals(2, check.validate("qwertyui"));
     }
 }
