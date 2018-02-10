a232f5f1a45cd674e79711941f6fbb031418e022
Di Wu <dz247633@dal.ca>
2018-02-10 09:34:24 -0400

    first

--- app/src/test/java/com/example/diwu/a2/ExampleUnitTest.java
+++ app/src/test/java/com/example/diwu/a2/ExampleUnitTest.java
@@ -14,20 +14,45 @@ public class ExampleUnitTest {
     public void check0() throws Exception {
         Validator check = new Validator();
 
-        assertEquals(0, check.validate(""));//none character
+        assertEquals(0, check.validate(""));
     }
 
     @Test
-    public void check1() {
+    public void test1(){
         Validator check = new Validator();
-        assertEquals(1, check.validate("password"));//String = "password" (8 characters)
-        assertEquals(1, check.validate("qwertyu"));//String have 7 characters
+        assertEquals(1, check.validate("password"));
+        assertEquals(1, check.validate("qwertyu"));
     }
 
     @Test
-    public void check2() {
+    public void test2(){
         Validator check = new Validator();
-        assertEquals(2, check.validate("Password"));//String = "password" upcase
-        assertEquals(2, check.validate("qwertyui"));// 8 characters
+        assertEquals(2, check.validate("Hello"));
+        assertEquals(2,check.validate("123456"));
+        assertEquals(2, check.validate("HFQUHF"));
+        assertEquals(2,check.validate("!@$#%"));
+        assertEquals(2,check.validate("qhfqhasdsja"));
+    }
+
+    @Test
+    public void test3(){
+        Validator check = new Validator();
+        assertEquals(3, check.validate("12345678"));
+        assertEquals(3, check.validate("QWERTYUI"));
+        assertEquals(3, check.validate("!@#$%^&*"));
+    }
+
+    @Test
+    public void test4(){
+        Validator check = new Validator();
+        assertEquals(4, check.validate("4321786QFUQHF"));
+        assertEquals(4,check.validate("837465ew!@#"));
+        assertEquals(4,check.validate("asdQWE!@#@*&^"));
+    }
+
+    @Test
+    public void test5(){
+        Validator check = new Validator();
+        assertEquals(5, check.validate("ADUFHdfjl%%%123sdaf"));
     }
 }
